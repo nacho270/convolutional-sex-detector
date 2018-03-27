97f4807e00e0021c51dd365bd11cf1f35ca35c6a
Ignacio Cicero <ignaciocicero@gmail.com>
2018-03-26 23:07:44 -0300

    Refactor flask startup and added POST method for the image

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/test', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+import rest
 
 if __name__ == '__main__':
-    app.run()
+   rest.startFlask()
