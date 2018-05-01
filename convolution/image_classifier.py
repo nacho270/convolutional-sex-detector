52fe32c135afedb61a5810bcdf1b07f44cd9a014
Ignacio Cicero <ignaciocicero@gmail.com>
2018-05-01 13:45:16 -0300

    Read image from http correctly

--- convolution/image_classifier.py
+++ convolution/image_classifier.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import sys
 import os
 
 # Disable tensorflow compilation warnings
@@ -7,35 +6,32 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def classify_image(image_data):
 
-    # TODO: CHECK SUPPORT FOR PNG
-    # https://stackoverflow.com/questions/34484148/feeding-image-data-in-tensorflow-for-transfer-learning
     # map to return prediction
     image_prediction = {}
 
-    # Loads label file, strips off carriage return
-    label_lines = [line.rstrip() for line
-                   in tf.gfile.GFile("/Users/nacho/Documents/dev/python/convolutional-sex-detector/convolution/tf_files/output_labels.txt")]
+    # Loads labels
+    label_lines = [line.rstrip() for line in tf.gfile.GFile("~/tf_files/output_labels.txt")]
 
-    # Unpersists graph from file
-    with tf.gfile.FastGFile("/Users/nacho/Documents/dev/python/convolutional-sex-detector/convolution/tf_files/output_graph.pb", 'rb') as f:
+    # Load model
+    with tf.gfile.FastGFile("~/tf_files/output_graph.pb", 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
 
     with tf.Session() as sess:
 
-        # Feed the image_data as input to the graph and get first prediction
+        # Load softmax tensor
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
+        # Analize image
         predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
 
-        # Sort to show labels of first prediction in order of confidence
+        # Sort results by probability
         top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
         for node_id in top_k:
-            human_string = label_lines[node_id]
+            image_class = label_lines[node_id]
             score = predictions[0][node_id]
-            image_prediction[human_string] = score
-            #print('%s (score = %.5f)' % (human_string, score))
+            image_prediction[image_class] = str(score)
 
-    return image_data
+    return image_prediction
--- rest/flask_starter.py
+++ rest/flask_starter.py
@@ -1,45 +1,23 @@
-from flask import Flask, jsonify, request, redirect,flash
-from convolution import test_tensorflow
+from flask import Flask, jsonify, request, redirect
 from convolution import image_classifier
 
 def start_flask():
 
     app = Flask(__name__)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello World!'
-
-    @app.route('/test', methods=['GET'])
+    @app.route('/ping', methods=['GET'])
     def test():
         return jsonify({
             'status': 'success',
             'message': 'pong!'
         })
 
-    @app.route('/tensor', methods=['GET'])
-    def ping_pong():
-        return jsonify({
-            'status': 'success',
-            'message': str(test_tensorflow.run_tf_exampe())
-        })
-
-    @app.route('/post', methods=['POST'])
-    def post_image():
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        return jsonify({
-            'status': 'success',
-            'message': 'found file' + file.filename
-        })
-
     @app.route('/classify', methods=['POST'])
     def classify_image():
         if 'image' not in request.files:
             return redirect(request.url)
-        image = request.files['image']
-        return jsonify(image_classifier.classify_image(image))
+        image = request.files.get('image', '')
+        return jsonify(image_classifier.classify_image(image.read()))
 
 
     app.run()
